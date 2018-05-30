a702ca24c03768bf0e3933023b46a24f18670b07
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2018-05-30 13:03:16 +0200

    User bundle

--- src/lib/View/UserForgotPasswordFormView.php
+++ src/lib/View/UserForgotPasswordFormView.php
@@ -3,6 +3,8 @@
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace EzSystems\EzPlatformUser\View;
 
 use eZ\Publish\Core\MVC\Symfony\View\BaseView;

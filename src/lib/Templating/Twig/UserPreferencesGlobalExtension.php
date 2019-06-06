313131216246cfe0a48d067898ac1b659267cb3f
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-06-06 15:53:23 +0200

    Move twig UserPreferencesGlobalExtension to ezplatform-user bundle

---
+++ src/lib/Templating/Twig/UserPreferencesGlobalExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformUser\Templating\Twig;
+
+use EzSystems\EzPlatformUser\UserSetting\UserSettingArrayAccessor;
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+
+/**
+ * @internal
+ */
+class UserPreferencesGlobalExtension extends AbstractExtension implements GlobalsInterface
+{
+    /** @var \EzSystems\EzPlatformUser\UserSetting\UserSettingArrayAccessor */
+    protected $userSettingArrayAccessor;
+
+    /**
+     * @param \EzSystems\EzPlatformUser\UserSetting\UserSettingArrayAccessor $userSettingArrayAccessor
+     */
+    public function __construct(
+        UserSettingArrayAccessor $userSettingArrayAccessor
+    ) {
+        $this->userSettingArrayAccessor = $userSettingArrayAccessor;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGlobals(): array
+    {
+        // has to use \ArrayAccess object due to BC promise
+
+        return [
+            'ez_user_settings' => $this->userSettingArrayAccessor,
+        ];
+    }
+}

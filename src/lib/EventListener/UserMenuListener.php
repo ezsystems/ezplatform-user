e75550a6069d79344566b3ff2bc475de33934cc1
Jakub <symfiz@gmail.com>
2019-09-24 12:55:30 +0200

    EZP-29618: Update user menu order and add icons (#46)

--- src/lib/EventListener/UserMenuListener.php
+++ src/lib/EventListener/UserMenuListener.php
@@ -10,7 +10,6 @@ namespace EzSystems\EzPlatformUser\EventListener;
 
 use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
 use JMS\TranslationBundle\Translation\TranslationContainerInterface;
-use Knp\Menu\Util\MenuManipulator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use JMS\TranslationBundle\Model\Message;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
@@ -51,13 +50,14 @@ class UserMenuListener implements EventSubscriberInterface, TranslationContainer
                 self::ITEM_CHANGE_PASSWORD,
                 [
                     'route' => 'ezplatform.user_profile.change_password',
-                    'extras' => ['translation_domain' => 'menu'],
+                    'extras' => [
+                        'translation_domain' => 'menu',
+                        'icon_url' => '/bundles/ezplatformadminui/img/ez-icons.svg#edit',
+                        'orderNumber' => 40,
+                    ],
                 ]
             );
         }
-
-        $manipulator = new MenuManipulator();
-        $manipulator->moveToPosition($menu[self::ITEM_CHANGE_PASSWORD], 0);
     }
 
     /**
